04b12d9721582a8c658785af4eae0d881d705b8c
sanex3339 <sanex3339@yandex.ru>
2016-05-07 14:26:26 +0300

    RC.1 update

--- resources/assets/typescript/vendor.ts
+++ resources/assets/typescript/vendor.ts
@@ -5,5 +5,16 @@ import 'es7-reflect-metadata/dist/browser';
 import 'zone.js/dist/zone';
 import 'zone.js/dist/long-stack-trace-zone';
 
+// Angular 2
+import '@angular/common';
+import '@angular/compiler';
+import '@angular/core';
+import '@angular/http';
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/router';
+import '@angular/router-deprecated';
+import '@angular/upgrade';
+
 // RxJS
 import 'rxjs';
